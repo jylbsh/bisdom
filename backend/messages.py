2342745d63eb9455278e88a2b5f11ef09352776c
大湯 <oyu@basenet.co.jp>
2025-01-28 11:13:09 +0900

    No.66 ナレッジ更新API実装 v2

--- backend/messages.py
+++ backend/messages.py
@@ -8,16 +8,16 @@ class Messages:
     message:str
 
 class ErrorMessages(Enum):
-    ERROR_ID_009E = '送信内容が存在していません'
-    ERROR_ID_008E = 'タイトルが未設定'
-    ERROR_ID_007E = 'ユーザーが未設定'
-    ERROR_ID_006E = 'htmlのサニタイジングが失敗しました'
-    ERROR_ID_005E = 'ナレッジID不正'
-    ERROR_ID_004E = 'XXX'
-    ERROR_ID_003E = 'ナレッジが見つかりませんでした'
-    ERROR_ID_002E = '{0}が埋め込んでない'
-    ERROR_ID_010E = Messages("010E","テスト",404)
     ERROR_ID_0001E = {'message':"test","id":"001E"}
+    ERROR_ID_0002E = '{0}が埋め込んでない'
+    ERROR_ID_0003E = 'ナレッジが見つかりませんでした'
+    ERROR_ID_0004E = 'XXX'
+    ERROR_ID_0005E = 'ナレッジID不正'
+    ERROR_ID_0006E = 'htmlのサニタイジングが失敗しました'
+    ERROR_ID_0007E = 'ユーザーが未設定'
+    ERROR_ID_0008E = 'タイトルが未設定'
+    ERROR_ID_0009E = '送信内容が存在していません'
+    ERROR_ID_0010E = Messages("010E","テスト",404)
     def __str__(self):
         return self.value
 class CreateBy(Enum):
